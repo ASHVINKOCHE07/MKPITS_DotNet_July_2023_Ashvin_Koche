131ed7edf5aff2860b2afde4a37b667ae0d9d3eb
ASHVINKOCHE07 <143704307+ASHVINKOCHE07@users.noreply.github.com>
2023-11-15 18:39:57 +0530

    program files

---
+++ c++/NumberofonesinthegivenNumber.cpp
@@ -0,0 +1,23 @@
+/*
+ * C++ Program to Count the Number of ones in the given Number
+ */
+ 
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int num, temp, count = 0;
+ 
+    cout << "Enter the number : ";
+    cin >> num;
+    temp = num;
+    while (temp != 0)
+    {
+        if (temp % 10 == 1)
+            count++;
+        temp = temp / 10;
+    }
+    cout << "The number of ones in " << num
+         << " is " << count << endl;
+}
---
+++ c++/SquareRootofanumberusingStandardLibrary.cpp
@@ -0,0 +1,29 @@
+/*
+ * C++ Program to Compute Square Root of a number using Standard Library
+ */
+#include <iostream>
+#include <cmath>
+#include <limits>
+ 
+int main()
+{
+    double output, input, min, max;
+ 
+    std::cout << "Enter the value : ";
+    std::cin  >> input;
+    std::cout << std::endl;
+    min = std::numeric_limits<double>::min();
+    max = std::numeric_limits<double>::max();
+    if (input >= min && input <= max)
+    {
+        output = sqrt(input);
+        std::cout << "Square root of " << input
+                  << " = " << output << std::endl;
+    }
+    else
+    {
+        std::cerr << "Error: " << input << " not within limits"
+                  << std::endl;
+    }
+    return 0;
+}
