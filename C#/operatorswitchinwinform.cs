e73c6d63488b84680a5c074469513a61a5cd8a78
ASHVINKOCHE07 <ashwindkoche@gmail.com>
2023-12-03 20:45:43 +0530

    programsfiles

---
+++ C#/operatorswitchinwinform.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1=Convert.ToInt32(textBox1.Text);
+            int num2=Convert.ToInt32(textBox2.Text);
+            String op=comboBox1.Text;
+            switch (op)
+            {
+                case "+":
+                    label4.Text = "addition = "+(num1+num2);
+                    break;
+                case "-":
+                    label4.Text = "subtraction = " + (num1 - num2);
+                    break;
+                case "*":
+                    label4.Text = "multiplication = " + (num1 * num2);
+                    break;
+                case "/":
+                    label4.Text = "Division = " + (num1 / num2);
+                    break;
+            }
+        }
+    }
+}
---
+++ C#/tableofnumberinwinform.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num= Convert.ToInt32(textBox1.Text);
+            int count = 1;
+            StringBuilder sb= new StringBuilder();  
+            while (count<= 10) 
+            {
+                sb.Append(num+"X"+count+"="+(num*count)+"\n");
+                count++;
+            }
+            label2.Text = sb.ToString();
+        }
+    }
+}
