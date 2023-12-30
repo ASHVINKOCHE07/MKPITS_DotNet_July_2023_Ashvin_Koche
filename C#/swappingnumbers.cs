069b097748bc80b112c40087d2566f434b99a3bb
ASHVINKOCHE07 <ashwindkoche@gmail.com>
2023-12-30 22:36:27 +0530

    programsfiles

---
+++ C#/swappingnumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //code for replacing two numbers with ADDITION.
+            int a = 10;  
+            int b = 20;
+            int temp = a + b;
+            a=temp-a;
+            Console.WriteLine("a={0}", a);
+           
+            b =temp-a;
+            Console.WriteLine("b={0}", b);
+            //code for replacing two numbers with MULTIPLICATION.
+            
+            temp = a*b;
+            a=temp/a;
+            Console.WriteLine("a={0}", a);
+            b = temp / a;
+            Console.WriteLine("b={0}", b);
+            Console.WriteLine("Bye");
+            Console.ReadKey();
+        }
+    }
+}
