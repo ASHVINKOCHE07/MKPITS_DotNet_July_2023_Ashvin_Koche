56a7ed57205bda250f5548f5e42eff15272e8df7
ASHVINKOCHE07 <ashwindkoche@gmail.com>
2024-01-02 22:31:50 +0530

    programsfiles

---
+++ WindowsForm/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //1-create connection string
+            string str = "server=LAPTOP-EL7M3D56\\SQLEXPRESS;integrated security=true;database=mksample";
+
+            SqlConnection con = null;
+
+            //2-use sql connection class to connect to sql database.
+            try
+            {
+                con = new SqlConnection(str);
+                con.Open();//in connected envi.
+                           //You have to explicitly open the connection
+                           //using open method.
+                MessageBox.Show("Connectiond Sucessfully !");
+            }
+            catch(Exception ee)
+            {
+                ee.ToString();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ WindowsForm/sqldatabaseconnect2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = DataBaseConnection.getConnection();
+            if (con != null)
+            {
+                MessageBox.Show("successfully connected");
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("could not connect to sqlserver");
+            }
+
+        }
+    }
+}
