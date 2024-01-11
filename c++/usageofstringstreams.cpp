f2baa87d8c43d44d863ce8761c8f5f9e177c5459
ASHVINKOCHE07 <ashwindkoche@gmail.com>
2024-01-11 22:13:09 +0530

    programfiles

---
+++ c++/usageofstringstreams.cpp
@@ -0,0 +1,20 @@
+/*
+ * C++ Program to illustrate usage of string streams
+ */
+#include <iostream>    
+#include <sstream>   
+#include <string>       
+using namespace std;
+ 
+int main () {
+    stringstream ss;
+    string s;
+ 
+    cout << "Enter your first name\n";
+    cin >> s;
+    ss.str(s);
+    cout << "Enter your last name\n";
+    cin >> s;
+    ss.str(ss.str() + " " + s);
+    cout << "Hello " << ss.str() << "!\n";
+}
